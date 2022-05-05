ad82060cdc83a04136591906d22feef3d3e23348
alexwkleung <alexleung25@outlook.com>
2022-05-05 15:12:59 -0700

    Updated comment

--- src/scripts/togglefullscreen.ts
+++ src/scripts/togglefullscreen.ts
@@ -1,4 +1,4 @@
-//credit: https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API -->
+//credit: https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
 function toggleFullScreen() {
     if(!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
