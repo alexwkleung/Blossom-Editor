66379bc5b1df9cd440928b5f1c896c76cc5501d2
alexwkleung <alexleung25@outlook.com>
2022-05-05 19:37:02 -0700

    Added open file button

---
+++ src/scripts/open-file.ts
@@ -0,0 +1,8 @@
+//credit: https://stackoverflow.com/questions/17646555/filereader-and-codemirror-load-file-complication
+function loadfile(input){
+    const reader = new FileReader()
+    reader.onload = function(e) {
+      editor.setValue(e.target.result);
+    }
+    reader.readAsText(input.files[0]);
+}
