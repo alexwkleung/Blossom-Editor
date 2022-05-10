ad1e6ad738fb138a97bd9d30f18ec2b5c9c178ca
alexwkleung <alexleung25@outlook.com>
2022-05-09 20:38:47 -0700

    Fixed spacing

--- src/scripts/hide.ts
+++ src/scripts/hide.ts
@@ -1,19 +1,19 @@
 function hideShowButtons() {
-    const hideButtonDiv = document.getElementById('hideButtons');
-    if(hideButtonDiv.style.display === "none") {
-      hideButtonDiv.style.display = "block";
-    } else {
-      hideButtonDiv.style.display = "none";
-    }
+  const hideButtonDiv = document.getElementById('hideButtons');
+  if(hideButtonDiv.style.display === "none") {
+    hideButtonDiv.style.display = "block";
+  } else {
+    hideButtonDiv.style.display = "none";
+  }
 };
 
 function hideShowPreview() {
-    const hidePrevDiv = document.getElementById('preview');
-    if(hidePrevDiv.style.display === "block") {
-        hidePrevDiv.style.display = "none";
-    } else {
-        hidePrevDiv.style.display = "block";
-    }
+  const hidePrevDiv = document.getElementById('preview');
+  if(hidePrevDiv.style.display === "block") {
+    hidePrevDiv.style.display = "none";
+  } else {
+     hidePrevDiv.style.display = "block";
+  }
 };
 
 function hideShowTerminal() {
